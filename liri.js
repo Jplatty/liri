18ab80bdb3a49ce59d7b5f5ceca571a8e7b290fc
TinTDang <tin2dang@gmail.com>
2018-08-20 19:29:48 -0500

    concert-this has results list

--- liri.js
+++ liri.js
@@ -32,45 +32,58 @@ function concert(input) {
 
     //link to api
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-    console.log(queryURL)
+
     request(queryURL, function (error, response, body) {
         if (error) {
-            error.red
+            log(error + "\r\n")
             return error.red
         }
         else if (!error && response.statusCode === 200) {
             //if band does not exist or Band does not perform anymore
             if (body.length < 20) {
-                log("Sorry! No results found.")
+                log("Sorry! No results found.+\r\n")
                 return console.log("Sorry! No results found");
             }
             //to make it easier to read
-            var call = JSON.parse(body)[0]
 
-            function printVenue() {
-                //outputs to log.txt
-                log("=================================================")
-                log(input + " is performing at " + call.venue.name)
-                log("At " + call.venue.city + ", " + location)
-                log("On " + moment(call.datetime, moment.ISO_8601).format("MM/DD/YYYY"))
-                log("=================================================")
+            
+            var call = JSON.parse(body)
+            var count = 1
+            // for how big the body results are
+            for (i = 0; i < call.length; i++) {
 
-                // outputs to terminal/bash
-                console.log("=================================================".blue)
-                console.log(input + " is performing at " + call.venue.name)
-                console.log("At " + call.venue.city + ", " + location)
-                console.log("On " + moment(call.datetime, moment.ISO_8601).format("MM/DD/YYYY"))
-                console.log("=================================================".blue)
+                var venueLocation = JSON.parse(body)[i]
+
+                if (venueLocation.venue.region === "") {
+                    var location =
+                     venueLocation.venue.country;
+                    log("Result: " + count)
+                    console.log("Result: " + count)
+                    printVenue();
+                } else {
+                    var location = venueLocation.venue.region;
+                    log("Result: " + count)
+                    console.log("Result: " + count)
+                    printVenue();
+                }
+
+                function printVenue() {
+                    // ouput to log.txt
+                    log("=================================================")
+                    log(input + " is performing at " + venueLocation.venue.name)
+                    log("At " + venueLocation.venue.city + ", " + location)
+                    log("On " + moment(venueLocation.datetime, moment.ISO_8601).format("MM/DD/YYYY") + "\r\n")
+
+                    // output to terminal/bash
+                    console.log("=================================================".blue)
+                    console.log(input + " is performing at " + venueLocation.venue.name)
+                    console.log("At " + venueLocation.venue.city + ", " + location)
+                    console.log("On " + moment(venueLocation.datetime, moment.ISO_8601).format("MM/DD/YYYY") + "\n")
+
+                }
+                count++
             }
 
-            // if there is no region, like canada for example
-            if (call.venue.region === "") {
-                var location = call.venue.country;
-                printVenue();
-            } else {
-                var location = call.venue.region;
-                printVenue();
-            }
         }
     })
 
@@ -81,6 +94,7 @@ function spotifySong(input) {
     // if they don't put in anything pull up "The Sign" by The Ace of Base
     if (input === '') {
         input = 'The Sign';
+        log("No input detected, defaulting search to \"The Sign\"\r\n")
         console.log("No input detected, defaulting search to \"The Sign\"".green)
     }
 
@@ -90,60 +104,72 @@ function spotifySong(input) {
         query: input
     }, function (err, data) {
         if (err) {
-            return err.red;
+            log(err + "\r\n");
+            return console.log(err.red);
 
         } else {
             // For easier readability
             var searchResults = data.tracks.items;
-            //Take the first 3 results
 
             if (searchResults.length < 20) {
-                return console.log("There were no results for ".red + input)
+                log("There were no results for " + input + "\r\n")
+                return console.log("There were no results for ".red + input.yellow)
             }
-                // notify user that there are 3 search results
-                console.log('There are 3 results based on your search on \''.cyan + input.yellow + "\'".cyan)
 
-                for (i = 0; i < 3; i++) {
+            // notify user that there are 3 search results
+            log('There are 3 results based on your search on \'' + input + "\'\r\n")
+            console.log('There are 3 results based on your search on \''.cyan + input.yellow + "\'".cyan)
 
-                    // created artist array for the case of multiple artists
-                    var artist = []
+            for (i = 0; i < 3; i++) {
+
+                // created artist array for the case of multiple artists
+                var artist = []
 
 
-                    console.log("======================================================================\n".blue)
+                log("======================================================================\r\n")
+                console.log("======================================================================\n".blue)
 
-                    //Artists, in the case for 1 artist
-                    if (searchResults[i].artists.length === 1) {
+                //Artists, in the case for 1 artist
+                if (searchResults[i].artists.length === 1) {
 
-                        console.log("Artist: " + searchResults[i].artists[0].name)
+                    log("Artist: " + searchResults[i].artists[0].name)
+                    console.log("Artist: " + searchResults[i].artists[0].name)
 
-                    } else { // If there are multiple artists
+                } else { // If there are multiple artists
 
-                        for (j = 0; j < searchResults[i].artists.length; j++) {
-                            //push into artist array
-                            artist.push(searchResults[i].artists[j].name)
-                        }
-                        //Then join the outputs in the array as a string with & in between each index
-                        console.log("Artists: " + artist.join(" & ").yellow)
+                    for (j = 0; j < searchResults[i].artists.length; j++) {
+                        //push into artist array
+                        artist.push(searchResults[i].artists[j].name)
                     }
-
-
-                    //Song Name
-                    console.log("\nSong Name: " + searchResults[i].name.yellow)
-
-
-                    // Preview Link
-                    // if preview_url is === null
-                    if (searchResults[i].preview_url === null) {
-                        console.log("\nSorry! No preview URL is available".red)
-                    } else {
-                        console.log("\nPreview URL: " + searchResults[i].preview_url.underline.blue)
-                    }
-
-                    // Album Name
-                    console.log("\nAlbum Name: " + searchResults[i].album.name.yellow.bold + "\n")
-
+                    //Then join the outputs in the array as a string with & in between each index
+                    log("Artists: " + artist.join(" & "))
+                    console.log("Artists: " + artist.join(" & ").yellow)
                 }
-            
+
+
+                //Song Name
+                log("\nSong Name: " + searchResults[i].name)
+                console.log("\nSong Name: " + searchResults[i].name.yellow)
+
+
+                // Preview Link
+                // if preview_url is === null
+                if (searchResults[i].preview_url === null) {
+                    log("\nSorry! No preview URL is available")
+                    console.log("\nSorry! No preview URL is available".red)
+                } else {
+                    log("\nPreview URL: " + searchResults[i].preview_url + "\n")
+                    console.log("\nPreview URL: " + searchResults[i].preview_url.underline.blue)
+                }
+
+                // Album Name
+                log("\nAlbum Name: " + searchResults[i].album.name + "\r\n")
+                console.log("\nAlbum Name: " + searchResults[i].album.name.yellow.bold + "\n")
+
+            }
+
+            log("======================================================================\r\n")
+
         }
     });
 };
@@ -152,6 +178,7 @@ function movie(input) {
 
     if (input === '') {
         input = 'Mr+Nobody';
+        log("No input detected, defaulting search to \"Mr. Nobody\"")
         console.log("No input detected, defaulting search to \"Mr. Nobody\"".green)
     }
     //redefine input as artist. if multiple spaces in between words, add a + for the query title
@@ -163,11 +190,25 @@ function movie(input) {
 
     request(queryURL, function (error, response, body) {
         if (error) {
+            log(error + "\r\n")
             return error
         }
         else if (!error && response.statusCode === 200) {
             //to make it easier to read
             var call = JSON.parse(body)
+            // output to log.txt
+            log("=================================================");
+            log("\n" + call.Title + "\n");
+            log("Year Released: " + call.Year);
+            log("IMDB Rating: " + call.imdbRating);
+            log("Produced in: " + call.Country);
+            log("Available languages: " + call.Language);
+            log("\nPlot: " + call.Plot);
+            log("Actors: " + call.Actors);
+            log("\n=================================================\r\n")
+
+
+            // output to terminal
             console.log("=================================================".blue);
             console.log("\n" + call.Title + "\n");
             console.log("Year Released: " + call.Year);
@@ -187,7 +228,8 @@ function movie(input) {
 
 function doWhatItSays() {
     fs.readFile("random.txt", "utf8", function (error, data) {
-        console.log(data)
+        log(data + "\r\n")
+        console.log(data.yellow)
         //split the data into an array
         var instruction = data.split(",");
         // [ 'spotify-this-song', '"I Want it That Way"' ]
@@ -215,6 +257,8 @@ function start() {
             break;
 
         default:
+            log("I could not recognize that command, please state one of these commands: ")
+            log("\nconcert-this" + "\nspotify-this-song" + "\nmovie-this" + "\ndo-what-it-says\r\n")
             console.log("I could not recognize that command, please state one of these commands: ".red)
             console.log("\nconcert-this" + "\nspotify-this-song" + "\nmovie-this" + "\ndo-what-it-says")
     }
@@ -225,14 +269,12 @@ logCommand()
 
 //print out a long
 function logCommand() {
-    fs.appendFile("log.txt", "Command: " + process.argv.slice(2).join(" ") + "\r\n", function (err) {
+    fs.appendFile("log.txt", "\r\nCommand: " + process.argv.slice(2).join(" ") + "\r\n\r\n", function (err) {
         if (err) {
             console.log(err.red);
-        } else {
-            console.log("\nCommand input is put into log.txt".green)
         }
     })
-    // console.log()
+    console.log("")
 }
 
 function log(print) {
